1f24689ff5a8ffc197ec2e2310212c5e8c3b4ed9
Greenford <lauren.n.greenford@gmail.com>
2020-01-30 18:10:13 -0600

    better timeout error handling

--- src/genius_scraper.py
+++ src/genius_scraper.py
@@ -4,6 +4,7 @@ from io import StringIO
 import sys, sqlite3, time
 import pandas as pd
 import traceback as tb
+from requests.exceptions import ReadTimeout
 
 class Capturing(list):
     def __enter__(self):
@@ -28,16 +29,8 @@ class Scraper:
         #df = pd.read_csv('data/MILLION_SONGS.csv', index_col=0)[['title', 'artist_name']]
         
         conn = sqlite3.connect('/mnt/snap/AdditionalFiles/track_metadata.db')
-        q = '''
-        SELECT track_id,
-            title, 
-            artist_name,
-            year
-        FROM songs
-        WHERE year >= 1958
-        ORDER BY year DESC;
-        '''
-            
+        q = '''SELECT track_id, title, artist_name, year FROM songs 
+               WHERE year >= 1958ORDER BY year DESC;'''
         df = pd.read_sql_query(q, conn)
         #df['title'] = df['title'].apply(lambda s: s[2:-1])
         #df['artist_name'] = df['artist_name'].apply(lambda s: s[2:-1])
@@ -50,7 +43,7 @@ class Scraper:
             try:
                 self.scrape_song_to_db(row['artist_name'],
                                        row['title'],
-                                       row['track_id'])
+                                       row['track_id'],)
             except TypeError as e:
                 self.record_error_verbose(self, row['track_id'], 
                         ''.join(tb.format_list(tb.extract_tb()))+f'\n {TypeError}: {e.args}')
@@ -58,24 +51,46 @@ class Scraper:
                 print(i)
 
     def scrape_song_to_db(self, artist, title, track_id):
-        with Capturing() as output:
-            songdata = self.api.search_song(title, artist)
-        if songdata == None:
-            if output[1].startswith('Timeout'):
-                time.sleep(60)
+        try:
+            #record stout from lyricsgenius call because it catches errors and prints
+            with Capturing() as output:
                 songdata = self.api.search_song(title, artist)
-                if songdata == None:
-                    self.record_error(track_id, 'Timeout')
-                else:
-                    self.record_lyrics_result(track_id, songdata)
-            elif output[1].startswith('No results'):
-                self.record_error(track_id, 'no_results')
-            elif output[1] == 'Specified song does not contain lyrics. Rejecting.':
-                self.record_error(track_id, 'lacks_lyrics')
-            elif output[1] == 'Specified song does not have a valid URL with lyrics. Rejecting.': 
-                self.record_error(track_id, 'invalid_url')
-        else:
+
+        #for the few errors that have been raised
+        except ReadTimeout as e:
+            self.api.sleep_time += 3
+            print(f'sleep time increased to {self.api.sleep_time}')
+            self.record_error(track_id, 'ReadTimeout')
+            self.scrape_song_to_db(artist, title, track_id)
+            return
+
+        #take sleep time slowly back to minimum
+        if self.api.sleep_time > self.minsleep:
+            self.api.sleep_time -= 0.25
+            print(f'sleep time decreased to {self.api.sleep_time}')
+        
+        #search successful
+        if songdata != None:
             self.record_lyrics_result(track_id, songdata)
+
+        #handle (record & retry) Timeout error
+        elif output[1].startswith('Timeout'):
+            self.api.sleep_time += 3 
+            self.record_error(track_id, 'Timeout')
+            self.scrape_song_to_db(artist, title, track_id)
+            return
+
+        #record error: not in genius db
+        elif output[1].startswith('No results'):
+            self.record_error(track_id, 'no_results')
+        
+        #record error: song without lyrics
+        elif output[1] == 'Specified song does not contain lyrics. Rejecting.':
+            self.record_error(track_id, 'lacks_lyrics')
+        
+        #record error: URL issue
+        elif output[1] == 'Specified song does not have a valid URL with lyrics. Rejecting.': 
+            self.record_error(track_id, 'invalid_url')
 
     def record_lyrics_result(self, track_id, songdata):
         self.lyrics.insert_one({
@@ -86,11 +101,12 @@ class Scraper:
 
     def record_error(self, track_id, errtype):
         self.errlog.insert_one({
-            '_id': track_id,
+            'track': track_id,
             'type': errtype})
+
     def record_error_verbose(self, track_id, errmsg):
         self.errlog.insert_one({
-            '_id': track_id,
+            'track': track_id,
             'type': 'verbose',
             'message': errmsg})
 
