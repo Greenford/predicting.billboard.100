62fa918f9913a10cd4a71367b8a921a85ef358a9
Ubuntu <ubuntu@ip-172-31-37-223.ec2.internal>
2020-02-03 15:05:47 +0000

    working first run

--- src/spotify_scraper.py
+++ src/spotify_scraper.py
@@ -1,16 +1,20 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from pymongo import MongoClient
+from pymongo.errors import BulkWriteError
+import pandas as pd
+import sqlite3, time, sys
 
 class Spotify_Scraper:
-    def __init__(sleeptime):
+    def __init__(self, sleeptime):
         self.sleeptime = sleeptime
-        client_credentials_manager = SpotifyClientCredentials()
-        with open('data/spotify.auth', 'r') as file:
-            client_id = file.read().strip()
-            client_secret = file.read().strip()
+        with open('data/spotify.auth', 'r') as f:
+            client_id = f.readline().strip()
+            client_secret = f.readline().strip()
 
-        client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-        self.sp = spotipy.Spotipy(client_credentials_manager=client_credentials_manager)
+        client_credentials_manager = SpotifyClientCredentials(client_id=client_id, 
+            client_secret=client_secret)
+        self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
         client = MongoClient('localhost', 27017)
         self.audio_features = client.tracks.audio_features
         self.audio_errlog = client.tracks.audio_errlog
@@ -21,10 +25,11 @@ class Spotify_Scraper:
                WHERE year >= 1958 ORDER BY year DESC;'''
         self.df = pd.read_sql_query(q, conn)
         
-        track_ids_with_lyrics = {track['_id'] for track in client.tracks.lyrics.find(
+        MSDIDs_with_lyrics = {track['_id'] for track in client.tracks.lyrics.find(
             {'_id':{'$exists':'true'}}, {'_id':'true'})}
-
-        self.df = df[df['track_id'] in track_ids_with_lyrics]
+        
+        mask = [(MSDID in MSDIDs_with_lyrics) for MSDID in self.df['track_id'].values] 
+        self.df = self.df[mask]
 
     def get_spotify_URI(self, artist, trackname):
         result = self.sp.search(f'artist:{artist} track:{trackname}')
@@ -38,26 +43,26 @@ class Spotify_Scraper:
 
     def scrape_all(self, verbose=0):
         i = 0
-        num_tracks self.df.shape[0]
-        while i < num_tracks
+        num_tracks = self.df.shape[0]
+        while i < num_tracks:
             prev_i = i
             tracks_arr = []
             err_arr = []
             while (i - prev_i < 50) & (i < num_tracks):
-                MSDID = row['track_id']
                 row = self.df.iloc[i]
+                MSDID = row['track_id']
                 trackdata = self.get_spotify_URI(row['artist_name'], row['title'])
                 if trackdata == None:
                     err_arr.append({'_id': MSDID, 'msg': 'No Spotify data'})
                 else:
-                    track_arr.append({'_id':MSDID, 'metadata': trackdata})
+                    tracks_arr.append({'_id':MSDID, 'metadata': trackdata})
                 i += 1
                 if verbose == 2:
                     print(i)
                 time.sleep(self.sleeptime)
             URIlist = list(map(lambda x: x['metadata']['uri'], tracks_arr))
             if verbose >= 1:
-                print('    {i}')
+                print(f'    {i}')
             af_arr = self.sp.audio_features(URIlist)
             self.insert_to_mongo(tracks_arr, af_arr, err_arr)
 
@@ -67,12 +72,17 @@ class Spotify_Scraper:
         for track in tracks_arr:
             URI = track['metadata']['uri']
             track['audio_features'] = af_arr[URI]
-        self.audio_features.insert_many(tracks_arr)
-        self.audio_errlog.insert_many(err_arr)
-
+        try:
+            self.audio_features.insert_many(tracks_arr, ordered=False)
+        except BulkWriteError as bwe:
+            print(bwe.details)
+        try:
+            self.audio_errlog.insert_many(err_arr)
+        except BulkWriteError as bwe:
+            print(bwe.details)
 if __name__ == '__main__':
     s = Spotify_Scraper(0.5)
-    s.scrape_all(verbose=True)
+    s.scrape_all(verbose=int(sys.argv[1]))
 
             
 
