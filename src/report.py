a3675fccc67e2a15806444c0c2cfa8ece57561cb
Ubuntu <ubuntu@ip-172-31-37-223.ec2.internal>
2020-02-03 19:47:19 +0000

    report.py now includes Spotify scraping report

--- src/report.py
+++ src/report.py
@@ -4,17 +4,25 @@ from pymongo import MongoClient
 class genius_report:
 
     def __init__(self):
-        client = MongoClient('localhost', 27017)
-        self.lyrics = client.tracks.lyrics
-        self.errlog = client.tracks.errlog
+        self.db = MongoClient('localhost', 27017).tracks
 
-    def num_scraped(self):
-        l = self.lyrics.estimated_document_count()
-        e = self.errlog.estimated_document_count()
-        print(f'{l} song lyrics gathered')
-        print(f'{e} errors encountered')
+    def num_lyrics_scraped(self):
+        l = self.db.lyrics.estimated_document_count()
+        e = self.db.errlog.estimated_document_count()
+        print('Genius:')
+        print(f'{l} song lyrics successfully gathered')
         print(f'{l+e} total tracks tried with {np.around(100*l/(l+e),2)}% success')
     
+    def num_spotify_tracks_scraped(self):
+        n = self.db.audio_features.estimated_document_count()
+        e = self.db.audio_errlog.estimated_document_count()
+        print('Spotify:')
+        print(f'{n} spotify track audio features successfully gathered')
+        print(f'{n+e} total tracks tried with {np.around(100*n/(l+e),2)}% success')
+
 if __name__ == '__main__':
     r = genius_report()
-    r.num_scraped()
+    r.num_lyrics_scraped()
+    print()
+    r.num_spotify_tracks_scraped()
+
