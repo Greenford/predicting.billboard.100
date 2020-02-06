6df5b457c15e2c5416b3a930a4c4f790b130352e
Greenford <lauren.n.greenford@gmail.com>
2020-02-05 20:25:04 -0600

    background sentiment analysis task added

--- src/my_nlp.py
+++ src/my_nlp.py
@@ -2,6 +2,7 @@ import nltk
 nltk.download('opinion_lexicon')
 from nltk.corpus import opinion_lexicon
 from nltk.tokenize import treebank
+from pymongo import MongoClient
 class Sentimenter:
     def __init__(self):
         self.pos = set(opinion_lexicon.positive())
@@ -17,7 +18,23 @@ class Sentimenter:
                 pcount += 1
             elif word in self.neg:
                 ncount += 1
-        return (pcount-ncount)/(pcount+ncount+1)
+        return (pcount-ncount)/(pcount+ncount+1), pcount, ncount
     
+    def process_mongo_docs():
+        newlabel='dict_sentiment'
+        collection = MongoClient('localhost', 27017).tracks.lyrics
+        results = collection.find({'_id':{'$exists':'true'}, 
+                                   'lyrics':{'$exists':'true'}, 
+                                   newlabel:{'$exists':'false'}},
+                                  {'_id':'true', 'lyrics':'true'})
+        s = Sentimenter()
+        count = 0
+        for track in results:
+            score = s.sentiment(track['lyrics'])
+            collection.update({'_id':track['_id']}, {'$set':{newlabel:score}})
+            count += 1
+            print(count)
 
+if __name__ == '__main__':
+    Sentimenter.process_mongo_docs()
 
